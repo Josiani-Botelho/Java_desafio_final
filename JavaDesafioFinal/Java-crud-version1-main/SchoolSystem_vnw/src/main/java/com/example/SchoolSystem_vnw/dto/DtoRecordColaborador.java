b6b3353aa215bb617a64ef7ab0cb6d8137551605
Josiani Botelho <91165149+Josiani-Botelho@users.noreply.github.com>
2024-04-05 20:10:46 -0300

    Add files via upload

---
+++ JavaDesafioFinal/Java-crud-version1-main/SchoolSystem_vnw/src/main/java/com/example/SchoolSystem_vnw/dto/DtoRecordColaborador.java
@@ -0,0 +1,12 @@
+package com.example.SchoolSystem_vnw.dto;
+
+
+import com.example.SchoolSystem_vnw.enums.EnumCargo;
+
+
+
+public record DtoRecordColaborador(String nome, String cpf, String email, EnumCargo cargo) {
+
+
+
+}
